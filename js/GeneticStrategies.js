d056611c1fdb07475d5b41cba9da1f09e05fd8b1
alessmolt <alessando7.molteni@mail.polimi.it>
2020-12-28 18:33:30 +0100

    Fixed Two point mating

--- js/GeneticStrategies.js
+++ js/GeneticStrategies.js
@@ -246,19 +246,20 @@ class SinglePointCrossover {
     compute(selected, crossoverProbability) {
         let N = selected.length;
         let offspring = selected.slice();
+        let crossoverPoint = Math.random();
 
         for(let i = 0; i <  N-1; i+=2){
             if(Math.random() < crossoverProbability){
 
-                offspring[i]  = this.singlePointMating(selected[i], selected[i+1]);
-                offspring[i+1]  = this.singlePointMating(selected[i+1], selected[i]);
+                offspring[i]  = this.singlePointMating(selected[i], selected[i+1], crossoverPoint);
+                offspring[i+1]  = this.singlePointMating(selected[i+1], selected[i], crossoverPoint);
             }
            
         }
         return offspring;
     }
 
-    singlePointMating(a, b, crossoverPoint = 0.5){
+    singlePointMating(a, b, crossoverPoint){
         let M = Math.floor(crossoverPoint * a.sequences.shape[1]);
         let firstChromosome = a.sequences.slice([null],[0, M]);
         let secondChromosome = b.sequences.slice([null], [M, b.sequences.shape[1]]);
@@ -275,32 +276,41 @@ class TwoPointCrossover {
      * @param {Array} selected Selected population to crossover
      * @param {float} crossoverProbability Probability to mating instead of surviving
      */
+
     compute(selected, crossoverProbability) {
         let N = selected.length;
         let offspring = selected.slice();
+        let start = Math.random();
+        let end = Math.random();
+        
+        if(start > end){
+            let tmp = end;
+            end = start;
+            start = tmp
+        }
 
         for(let i = 0; i <  N-1; i+=2){
             if(Math.random() < crossoverProbability){
 
-                offspring[i] = this.twoPointMating(selected[i], selected[i+1]);
-                offspring[i+1] = this.twoPointMating(selected[i+1], selected[i]);
+                offspring[i] = this.twoPointMating(selected[i], selected[i+1], start, end);
+                offspring[i+1] = this.twoPointMating(selected[i+1], selected[i],start, end);
             }
         }
         return offspring;
     }
 
-    /*
-    FIXME: ritorna array vuoti
-    */
+
     twoPointMating(a, b, start, end){
-        let startingIndex = Math.floor(start * a.sequences.shape[1]);
-        let finalIndex = Math.floor(end * a.sequences.shape[1]);
-        let firstChromosome = a.sequences.slice([null],[0, startingIndex]);
-        let secondChromosome = b.sequences.slice([null],[startingIndex, finalIndex]);
-        let thirdChromosome = b.sequences.slice([null],[finalIndex, b.sequences.shape[1]]);
         let child = new Pattern();
-        let seq = nj.concatenate(firstChromosome, secondChromosome);
-        child.sequences = nj.concatenate(seq, thirdChromosome);
+        if (start <= end){
+            let startingIndex = Math.floor(start * a.sequences.shape[1]);
+            let finalIndex = Math.floor(end * a.sequences.shape[1]);
+            let firstChromosome = a.sequences.slice([null],[0, startingIndex]);
+            let secondChromosome = b.sequences.slice([null],[startingIndex, finalIndex]);
+            let thirdChromosome = b.sequences.slice([null],[finalIndex, b.sequences.shape[1]]);
+            let seq = nj.concatenate(firstChromosome, secondChromosome);
+            child.sequences = nj.concatenate(seq, thirdChromosome);
+        }
         return child
     }
 }
