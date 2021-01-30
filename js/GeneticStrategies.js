180bd1f0f54bc8d796adfc804add1e84639a1470
alessmolt <alessando7.molteni@mail.polimi.it>
2021-01-30 11:37:33 +0100

    added strategies

--- js/GeneticStrategies.js
+++ js/GeneticStrategies.js
@@ -6,7 +6,10 @@ class FitnessStrategyManager {
 
     constructor() {
         this._strategies = [
-            new FitnessBEH()
+            new FitnessBEH(),
+            new FitnessBalance(),
+            new FitnessEvenness(),
+            new FitnessEntropy()
         ];
     }
 
@@ -14,6 +17,242 @@ class FitnessStrategyManager {
         return this._strategies.find(strategy => strategy._name === name);
     }
 
+}
+
+class FitnessEntropy {
+    _name = "Fitness Entropy"
+    constructor() {}
+
+    /**
+     * Utility function to compute the distance
+     * from one onset from the others
+     * @param {Array} p Pattern in p notation 
+     */
+    distance(p) {
+        let ons = []
+        let tmp = []
+        let N = p.length;
+
+        for (let i = 0; i < N; i++){
+            if (p[i]!==0){
+                ons.push(i);
+            }
+        }
+
+        if(ons.length > 0){
+            ons.push(N+ons[0]);
+        }
+        else ons.push(N);
+        
+        for (let i = 0; i < ons.length-1; i++){ 
+            tmp.push(ons[i+1] - ons[i]);
+        }
+
+        return tmp;
+    }
+
+    countOccurrences(array) {
+        const countOccurrences = arr => arr.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {});
+        return countOccurrences(array)
+    }
+    /**
+     * Utility function to compute the Inter Onset Interval
+     * @param {Array} p Pattern in p notation 
+     */
+    IOI(p){
+        let g = this.distance(p);
+        let occurrencesDict = this.countOccurrences(g);
+
+        //N is the number of intervals (== number of onsets)
+        let N = 0;
+        Object.keys(occurrencesDict).forEach(function(key) {
+            N += occurrencesDict[key];
+         });
+        
+        // Len of pattern + 1, taking into account seq with only one onset (distance 16 needs index 16)
+        let ans = Array(p.length+1).fill(0)
+        Object.entries(occurrencesDict).forEach(function(pair) {
+            ans[pair[0]] = pair[1] / N
+        });
+        
+        return ans;
+    }
+
+    entropy(ioi) {
+        let N = ioi.length
+        let sum = 0;
+        let eps = 0.00000000001
+        for(let i=0; i < N; ++i) {
+            sum += (ioi[i]*Math.log(ioi[i]+eps))
+        }
+        return - sum / Math.log(N);
+    }
+
+
+    /**
+     * Fitness Function
+     * @param {Array} pattern Pattern 
+     */
+    compute(pattern) {
+        let list = pattern.sequences.tolist();
+        let h = []; 
+        for (let i = 0; i < list.length; i++){
+            let p = list[i];
+            let ioi = this.IOI(p);
+            h.push(this.entropy(ioi));
+
+        }
+
+        return h;
+    }
+}
+
+class FitnessEvenness {
+    _name = "Fitness Evenness"
+    constructor() {}
+
+    /**
+     * Function to convert from p notation to x notation
+     * @param {Array} p Array of onsets in p notation
+     */
+    p2x(p) {
+        let result = []
+        let N = p.length
+        for (let i = 0; i < N; i++){
+            if (p[i]!==0){
+                result.push(i/N);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Function to convert from x notation to z notation
+     * @param {Array} x Array of onsets in x notation
+     */
+    x2z(x) {
+        let z = [];
+        for (let i = 0; i < x.length; i++){
+            let twoPiJ = math.complex(0,2*math.pi);
+            z.push(math.exp(math.multiply(x[i], twoPiJ)))
+        }
+        return z;
+    }
+
+    /**
+     * Compute evenness of pattern
+     * @param {Array} z Pattern in z notation
+     */
+    evenness(z){
+        let N = z.length;
+        if (N == 0) return 0
+        let e = 0
+        for (let k=0; k < z.length; ++k) {
+            let minusTwoPij = math.complex(0, -2*math.pi);
+            let exponent = math.multiply(minusTwoPij, k);
+            exponent = math.divide(exponent, N)
+            let tmp = math.multiply(z[k], math.exp(exponent))
+            e = math.add(e, tmp)
+        }
+        e = math.abs(e)
+        e = math.divide(e,N)
+        return e;
+    }
+
+    /**
+     * Fitness Function
+     * @param {Array} pattern Pattern 
+     */
+    compute(pattern) {
+        let list = pattern.sequences.tolist();
+        let e = [];
+        for (let i = 0; i < list.length; i++){
+            let p = list[i];
+            let x = this.p2x(p);
+            let z = this.x2z(x);
+            e.push(this.evenness(z));
+        }
+
+        return e;
+    }
+}
+
+
+class FitnessBalance {
+    _name = "Fitness Balance"
+    constructor() {}
+
+    /**
+     * Function to convert from p notation to x notation
+     * @param {Array} p Array of onsets in p notation
+     */
+    p2x(p) {
+        let result = []
+        let N = p.length
+        for (let i = 0; i < N; i++){
+            if (p[i]!==0){
+                result.push(i/N);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Function to convert from x notation to z notation
+     * @param {Array} x Array of onsets in x notation
+     */
+    x2z(x) {
+        let z = [];
+        for (let i = 0; i < x.length; i++){
+            let twoPiJ = math.complex(0,2*math.pi);
+            z.push(math.exp(math.multiply(x[i], twoPiJ)))
+        }
+        return z;
+    }
+
+    /**
+     * Utility funcution to compute the sum over the array
+     * @param {Array} z Array to be summed
+     */
+    summation(z){
+        let sum = math.complex(0, 0);
+        for (let i = 0; i < z.length; i++){
+            sum  = math.add(sum, z[i]);
+        }
+        return sum;
+    }
+
+    /**
+     * Compute balance of pattern
+     * @param {Array} z Pattern in z notation
+     */
+    balance(z){
+        let N = z.length; 
+        if (N === 0){
+            return 0
+        }
+
+        else{
+            return 1 - math.abs(this.summation(z))/N
+        }
+    }
+
+    /**
+     * Fitness Function
+     * @param {Array} pattern Pattern 
+     */
+    compute(pattern) {
+        let list = pattern.sequences.tolist();
+        let b = [];
+        for (let i = 0; i < list.length; i++){
+            let p = list[i];
+            let x = this.p2x(p);
+            let z = this.x2z(x);
+            b.push(this.balance(z));
+        }
+
+        return b;
+    }
 }
 
 class FitnessBEH {
@@ -167,12 +406,16 @@ class FitnessBEH {
      */
     compute(pattern) {
         let list = pattern.sequences.tolist();
+        let beh = [];
         for (let i = 0; i < list.length; i++){
-            p = list[i];
-            x = p2x(p);
+            let p = list[i];
+            let x = this.p2x(p);
+            let z = this.x2z(x);
+            let ioi = this.IOI(p);
+            beh.push(this.balance(z) * this.evenness(z) * this.entropy(ioi));
         }
 
-        return score;
+        return beh;
     }
 }
 
@@ -347,7 +590,9 @@ class MutationStrategyManager {
 
     constructor() {
         this._strategies = [
-            new MutationStrategy1()
+            new MutationStrategy1(),
+            new FlipBitMutation(),
+
         ];
     }
 
@@ -355,6 +600,47 @@ class MutationStrategyManager {
         return this._strategies.find(strategy => strategy._name === name);
     }
 }
+
+class FlipBitMutation {
+    _name = "Flip Bit";
+    constructor() {}
+
+    /**
+     * @param {Array} mutated Population after mutation
+     * @param {float} mutationProbability Probability of mutation
+     */
+    compute(offspring, mutationProbability) {
+        let N = offspring.length;
+        let mutated = offspring.slice;
+   
+        for(let i = 0; i < N; i++){
+            if(Math.random() < mutationProbability){
+
+                mutated[i]  = this.flipBit(offspring[i]);
+                
+            }
+           
+        }
+
+        return mutated;
+    }
+
+    flipBit(p){
+        let N = p.steps;
+        let num_seq = p.numSequences;
+
+        for (let i = 0; i < num_seq; i++){
+            for(let j = 0; j < N; j++){
+                if(p.sequences.get(i, j) == 1)
+                    p.sequences.set(i, j, 0);
+                else
+                    p.sequences.set(i, j, 1);        
+            }
+        }
+        return p;
+    }
+}
+
 
 class MutationStrategy1 {
     _name = "MutationStrategy1";
@@ -366,7 +652,7 @@ class MutationStrategy1 {
      */
     compute(offspring, mutationProbability) {
         let N = offspring.length;
-        let mutated = offspring.slice();
+        let mutated = offspring.slice;
    
         for(let i = 0; i < N; i++){
             if(Math.random() < mutationProbability){
@@ -376,7 +662,7 @@ class MutationStrategy1 {
             }
            
         }
-    
+
         return mutated;
     }
 
