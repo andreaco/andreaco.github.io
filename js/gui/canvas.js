78e43f8379c22c53a6074124f266166f2edf0b37
Andrea Coppola <andrea.cppl@gmail.com>
2021-02-13 12:11:11 +0100

    Added evenness representation, polished the canvas

--- js/gui/canvas.js
+++ js/gui/canvas.js
@@ -1,6 +1,6 @@
-let kickColor  = '#f2711c'
-let snareColor = '#2185d0'
-let hihatColor = '#a333c8'
+let kickColor  = '#f2711caf'
+let snareColor = '#2185d0af'
+let hihatColor = '#a333c8af'
 
 
 function drawEuclideanBase(ctx, w, h, radius, seq) {
@@ -69,11 +69,11 @@ function drawSequences(count) {
     var h = canvas.height;
     var status = count / kick_pattern.length
 
-    var radius = 0.45 * w
+    var radius = 0.36 * w
     var center = [h / 2, w / 2]
 
     //Backgroun clear 
-    ctx.clearRect(0, 0, w, h, radius);
+    ctx.clearRect(0, 0, w, h);
     drawEuclideanBase(ctx, w, h, radius, kick_pattern)
     drawEuclideanSteps(ctx, w, h, radius, count, kick_pattern, kickColor)
 
@@ -84,32 +84,7 @@ function drawSequences(count) {
     drawEuclideanSteps(ctx, w, h, radius * 0.4, count, hihat_pattern, hihatColor)
 }
 
-function drawBalance() {
-    // Canvas vars
-    var canvas = document.getElementById('balance');
-    var ctx = canvas.getContext('2d');
 
-    // Utility vars
-    var w = canvas.width;
-    var h = canvas.height;
-
-    var radius = 0.45 * w
-    var center = [h / 2, w / 2]
-
-    ctx.clearRect(0, 0, w, h, radius);
-
-    // Outer circle
-    drawEuclideanBase(ctx, w, h, radius, kick_pattern)
-
-    drawBalanceCenter(ctx, w, h, radius, kick_pattern, kickColor)
-    drawBalanceCenter(ctx, w, h, radius, snare_pattern, snareColor)
-    drawBalanceCenter(ctx, w, h, radius, hihat_pattern, hihatColor)
-
-    drawBalanceSteps(ctx, w, h, radius, kick_pattern, kickColor, 14)
-    drawBalanceSteps(ctx, w, h, radius, snare_pattern, snareColor, 10)
-    drawBalanceSteps(ctx, w, h, radius, hihat_pattern, hihatColor, 6)
-
-}
 
 function drawBalanceCenter(ctx, w, h, radius, seq, color) {
     var center = [h / 2, w / 2]
@@ -132,7 +107,7 @@ function drawBalanceCenter(ctx, w, h, radius, seq, color) {
     balanceX /= N
     balanceY /= N
     ctx.beginPath();
-    ctx.arc(balanceX, balanceY, 10, 0, 2 * Math.PI, false);
+    ctx.arc(balanceX, balanceY, 5, 0, 2 * Math.PI, false);
     ctx.fillStyle = color;
     ctx.fill();
 
@@ -166,6 +141,84 @@ function drawBalanceSteps(ctx, w, h, radius, seq, color, size) {
     }
 }
 
+
+function drawBalance() {
+    // Canvas vars
+    var canvas = document.getElementById('balance');
+    var ctx = canvas.getContext('2d');
+
+    // Utility vars
+    var w = canvas.width;
+    var h = canvas.height;
+
+    var radius = 0.36 * w
+    var center = [h / 2, w / 2]
+
+    ctx.clearRect(0, 0, w, h);
+
+    let kick_balance  = trimValue(balance(x2z(p2x(kick_pattern))));
+    let snare_balance = trimValue(balance(x2z(p2x(snare_pattern))));
+    let hihat_balance = trimValue(balance(x2z(p2x(hihat_pattern))));
+
+    ctx.font = "17px Arial";
+    
+    ctx.fillStyle = kickColor;
+    ctx.fillText(kick_balance  + "%", w *  2 / 7 + 8, 20); 
+    ctx.fillStyle = snareColor;
+    ctx.fillText(snare_balance + "%", w *  3 / 7 + 8, 20); 
+    ctx.fillStyle = hihatColor;
+    ctx.fillText(hihat_balance + "%", w *  4 / 7 + 8, 20); 
+
+
+
+    // Outer circle
+    drawEuclideanBase(ctx, w, h, radius, kick_pattern)
+
+    drawBalanceCenter(ctx, w, h, radius, kick_pattern, kickColor)
+    drawBalanceCenter(ctx, w, h, radius, snare_pattern, snareColor)
+    drawBalanceCenter(ctx, w, h, radius, hihat_pattern, hihatColor)
+
+    drawBalanceSteps(ctx, w, h, radius, kick_pattern, kickColor, 13)
+    drawBalanceSteps(ctx, w, h, radius, snare_pattern, snareColor, 9)
+    drawBalanceSteps(ctx, w, h, radius, hihat_pattern, hihatColor, 5)
+
+}
+
+function drawEvennessPolygon(ctx, w, h, radius, color, seq) {
+    let center = [h / 2, w / 2]
+    let theta = 0
+    let x = center[0] + Math.cos(theta) * radius
+    let y = center[1] + Math.sin(theta) * radius
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 3;
+    let firstPoint = -1;
+    for (let i = 0; i < seq.length; ++i) {
+        if(seq[i]) {
+            if (firstPoint == -1) firstPoint = i
+
+            theta = 2 * Math.PI * i / seq.length
+            x = center[0] + Math.cos(theta) * radius
+            y = center[1] + Math.sin(theta) * radius
+
+            ctx.lineTo(x, y);
+        }
+
+    }
+    if (firstPoint != -1) {
+        theta = 2 * Math.PI * firstPoint / seq.length
+        x = center[0] + Math.cos(theta) * radius
+        y = center[1] + Math.sin(theta) * radius
+
+        ctx.lineTo(x, y);
+    }
+    ctx.stroke();
+}
+
+function trimValue(val) {
+    return Math.floor(100*val);
+}
+
 function drawEvenness() {
     // Canvas vars
     var canvas = document.getElementById('evenness');
@@ -174,21 +227,36 @@ function drawEvenness() {
     // Utility vars
     var w = canvas.width;
     var h = canvas.height;
+    
+    var radius = 0.36 * w;
 
-    var radius = 0.45 * w;
-    var center = [h / 2, w / 2];
+    ctx.clearRect(0, 0, w, h);
+    
+    let kick_evenness = trimValue(evenness(x2z(p2x(kick_pattern))));
+    let snare_evenness = trimValue(evenness(x2z(p2x(snare_pattern))));
+    let hihat_evenness= trimValue(evenness(x2z(p2x(hihat_pattern))));
 
-    ctx.clearRect(0, 0, w, h, radius);
+    ctx.font = "17px Arial";
+    
+    ctx.fillStyle = kickColor;
+    ctx.fillText(kick_evenness  + "%", w *  2 / 7 + 8, 20); 
+    ctx.fillStyle = snareColor;
+    ctx.fillText(snare_evenness + "%", w *  3 / 7 + 8, 20); 
+    ctx.fillStyle = hihatColor;
+    ctx.fillText(hihat_evenness + "%", w *  4 / 7 + 8, 20); 
 
-    let kick_evenness = evenness(x2z(p2x(kick_pattern)));
-    let snare_evenness = evenness(x2z(p2x(snare_pattern)));
-    let hihat_evenness= evenness(x2z(p2x(hihat_pattern)));
-    let tot_evenness = (kick_evenness + snare_evenness + hihat_evenness)/3;
 
-    drawColumn(ctx, w, h, w / 5, kick_evenness, w * 0 / 4, kickColor);
-    drawColumn(ctx, w, h, w / 5, snare_evenness, w * 1 / 4, snareColor);
-    drawColumn(ctx, w, h, w / 5, hihat_evenness, w * 2 / 4, hihatColor);
-    drawColumn(ctx, w, h, w / 5, tot_evenness, w * 3 / 4, "#00aaaa");
+    drawEuclideanBase(ctx, w, h, radius * 0.4, hihat_pattern)
+    drawEvennessPolygon(ctx, w, h, radius * 0.4, hihatColor, hihat_pattern)
+    drawEuclideanSteps(ctx, w, h, radius * 0.4, -1, hihat_pattern, hihatColor)
+
+    drawEuclideanBase(ctx, w, h, radius * 0.7, snare_pattern)
+    drawEvennessPolygon(ctx, w, h, radius * 0.7, snareColor, snare_pattern)
+    drawEuclideanSteps(ctx, w, h, radius * 0.7, -1, snare_pattern, snareColor)
+
+    drawEuclideanBase(ctx, w, h, radius, kick_pattern)
+    drawEvennessPolygon(ctx, w, h, radius, kickColor,  kick_pattern)
+    drawEuclideanSteps(ctx, w, h, radius, -1, kick_pattern, kickColor)
 }
 
 function drawEntropy() {
@@ -200,21 +268,37 @@ function drawEntropy() {
     var w = canvas.width;
     var h = canvas.height;
 
-    var radius = 0.45 * w;
-    var center = [h / 2, w / 2];
+    ctx.clearRect(0, 0, w, h);
 
-    ctx.clearRect(0, 0, w, h, radius);
-
-    let kick_entropy = entropy(IOI(kick_pattern));
+    let kick_entropy  = entropy(IOI(kick_pattern));
     let snare_entropy = entropy(IOI(snare_pattern));
-    let hihat_entropy= entropy(IOI(hihat_pattern));
+    let hihat_entropy = entropy(IOI(hihat_pattern));
     let tot_entropy = (kick_entropy + snare_entropy + hihat_entropy)/3;
 
-    drawColumn(ctx, w, h, w / 5, kick_entropy, w * 0 / 4, kickColor);
+    // Outlines
+    ctx.strokeStyle = '#434c5e';
+    ctx.strokeRect(w * 0 / 4, 0, w / 5, h);
+    ctx.strokeRect(w * 1 / 4, 0, w / 5, h);
+    ctx.strokeRect(w * 2 / 4, 0, w / 5, h);
+    ctx.strokeRect(w * 3 / 4, 0, w / 5, h);
+
+    
+    // Fill
+    drawColumn(ctx, w, h, w / 5, kick_entropy,  w * 0 / 4, kickColor);
     drawColumn(ctx, w, h, w / 5, snare_entropy, w * 1 / 4, snareColor);
     drawColumn(ctx, w, h, w / 5, hihat_entropy, w * 2 / 4, hihatColor);
-    drawColumn(ctx, w, h, w / 5, tot_entropy, w * 3 / 4, "#00aaaa");
+    drawColumn(ctx, w, h, w / 5, tot_entropy,   w * 3 / 4, "#00aaaa");
+
+
+    ctx.font = "17px Arial";
+    
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(trimValue(kick_entropy)  + "%",  w/20 + w *  0 / 4, h-10); 
+    ctx.fillText(trimValue(snare_entropy) + "%",  w/20 + w *  1 / 4, h-10); 
+    ctx.fillText(trimValue(hihat_entropy) + "%",  w/20 + w *  2 / 4, h-10); 
+    ctx.fillText(trimValue(tot_entropy)   + "%",   w/20 + w *  3 / 4, h-10); 
 }
+
 
 
 function drawColumn(ctx, w, h, rectwidth, amount, xstart, color) {
@@ -224,6 +308,8 @@ function drawColumn(ctx, w, h, rectwidth, amount, xstart, color) {
     ctx.rect(xstart, ystart, rectwidth, h - ystart);
     ctx.fill();
 }
+
+
 
 function draw(count) {
     drawSequences(count);
